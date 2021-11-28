90c0e481ea16599b0c254a72c3c4ea78bac27457
Aleksandr Saitgalin <cred1tor@yandex.ru>
2021-11-29 01:54:04 +0600

    add 'id' property to User

--- server/entities/User.js
+++ server/entities/User.js
@@ -1,10 +1,13 @@
 import encrypt from '../utils/encrypt.js';
 
 export default class User {
+  static id = 1;
   guest = false;
   admin = false;
 
   constructor(email, name, password) {
+    this.id = this.constructor.id;
+    this.constructor.id += 1;
     this.email = email;
     this.name = name;
     this.passwordDigest = encrypt(password);
