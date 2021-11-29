447c4d38e2cdf705e8434f861f4c0f2fd7155183
Aleksandr Saitgalin <cred1tor@yandex.ru>
2021-11-29 16:26:22 +0600

    add add views setting and __dirname variable

--- server/index.js
+++ server/index.js
@@ -1,13 +1,21 @@
 import Express from 'express';
 import session from 'express-session';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import addRoutes from './routes/index.js';
 import User from './entities/User.js';
 import Admin from './entities/Admin.js';
+
+// eslint-disable-next-line no-underscore-dangle
+const __filename = fileURLToPath(import.meta.url);
+// eslint-disable-next-line no-underscore-dangle
+const __dirname = path.dirname(__filename);
 
 export default () => {
   const app = new Express();
 
   app.set('view engine', 'pug');
+  app.set('views', path.join(__dirname, 'views'));
   app.use(session({
     secret: 'secret key',
     resave: false,
@@ -21,6 +29,7 @@ export default () => {
     new User('user@user', 'pepe the frog', '123'),
   ];
 
+  app.models = {};
   app.models.users = users;
 
   return app;
