933a25077cd5c57eac9bd766396d282c58c991e8
Aleksandr Saitgalin <cred1tor@yandex.ru>
2021-11-29 16:40:19 +0600

    add session route module

---
+++ server/routes/session.js
@@ -0,0 +1,24 @@
+export default (app) => {
+  app.get('/session/new', (req, res) => {
+    res.render('session/new', { form: {}, errors: {} });
+  });
+
+  app.post('/session', (req, res) => {
+    const { email, password } = req.body;
+    const user = app.models.users.find((u) => u.email === email);
+
+    if (!user || !user.verifyPassword(password)) {
+      const error = 'Invalid email or password';
+      res.status(422).render('session/new', { form: req.body, error });
+      return;
+    }
+
+    req.session.email = email;
+    res.redirect('/');
+  });
+
+  app.delete('/session', (req, res) => {
+    req.session.destroy();
+    res.redirect('/');
+  });
+};
