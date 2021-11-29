180c5850ba54b7d0a050527b4e91910ff287ecef
Aleksandr Saitgalin <cred1tor@yandex.ru>
2021-11-29 15:37:33 +0600

    add view engine and session configuration

--- server/index.js
+++ server/index.js
@@ -1,10 +1,19 @@
 import Express from 'express';
+import session from 'express-session';
 import addRoutes from './routes/index';
 import User from './entities/User';
 import Admin from './entities/Admin';
 
 export default () => {
   const app = new Express();
+
+  app.set('view engine', 'pug');
+  app.use(session({
+    secret: 'secret key',
+    resave: false,
+    saveUninitialized: false,
+  }));
+
   addRoutes(app);
 
   const users = [
