b12684159e653183facba30ff4bd7ecc6c6f9454
Aleksandr Saitgalin <cred1tor@yandex.ru>
2021-11-30 15:03:06 +0600

    add authCookie to all tests where needed

--- __tests__/topics.test.js
+++ __tests__/topics.test.js
@@ -5,8 +5,6 @@ import signIn from './helpers/signIn.js';
 
 const { default: matchers } = jestSupertestMatchers;
 
-const signInCookie = signIn(getApp(), { email: 'admin@admin', password: 'qwerty' });
-
 describe('requests', () => {
   beforeAll(() => {
     expect.extend(matchers);
@@ -18,9 +16,11 @@ describe('requests', () => {
   });
 
   it('GET /topics/new', async () => {
-    const res = await request(getApp())
+    const app = getApp();
+    const authCookie = await signIn(app, { email: 'admin@admin', password: 'qwerty' });
+    const res = await request(app)
       .get('/topics/new')
-      .set('Cookie', signInCookie);
+      .set('Cookie', authCookie);
     expect(res).toHaveHTTPStatus(200);
   });
 
@@ -31,26 +31,32 @@ describe('requests', () => {
   });
 
   it('POST /topics', async () => {
-    const res = await request(getApp())
+    const app = getApp();
+    const authCookie = await signIn(app, { email: 'admin@admin', password: 'qwerty' });
+    const res = await request(app)
       .post('/topics')
       .type('form')
-      .set('Cookie', signInCookie)
+      .set('Cookie', authCookie)
       .send({ title: 'topic title', body: 'topic body' });
     expect(res).toHaveHTTPStatus(302);
   });
 
   it('POST /topics (errors)', async () => {
-    const res = await request(getApp())
-      .post('/topics');
+    const app = getApp();
+    const authCookie = await signIn(app, { email: 'admin@admin', password: 'qwerty' });
+    const res = await request(app)
+      .post('/topics')
+      .set('Cookie', authCookie);
     expect(res).toHaveHTTPStatus(422);
   });
 
   it('GET topics/:id', async () => {
     const app = getApp();
+    const authCookie = await signIn(app, { email: 'admin@admin', password: 'qwerty' });
     const res1 = await request(app)
       .post('/topics')
       .type('form')
-      .set('Cookie', signInCookie)
+      .set('Cookie', authCookie)
       .send({ title: 'topic title', body: 'topic body' });
     expect(res1).toHaveHTTPStatus(302);
     const url = res1.headers.location;
@@ -61,54 +67,58 @@ describe('requests', () => {
 
   it('PATCH topics/:id', async () => {
     const app = getApp();
+    const authCookie = await signIn(app, { email: 'admin@admin', password: 'qwerty' });
     const res1 = await request(app)
       .post('/topics')
       .type('form')
-      .set('Cookie', signInCookie)
+      .set('Cookie', authCookie)
       .send({ title: 'topic title', body: 'topic body' });
     const url = res1.headers.location.match(/\/topics\/\d+/)[0];
     const res2 = await request(app)
       .patch(url)
       .type('form')
-      .set('Cookie', signInCookie)
+      .set('Cookie', authCookie)
       .send({ title: 'new topic title', body: 'new topic body' });
     expect(res2).toHaveHTTPStatus(302);
   });
 
   it('PATCH topics/:id (unproccessable entity)', async () => {
     const app = getApp();
+    const authCookie = await signIn(app, { email: 'admin@admin', password: 'qwerty' });
     const res1 = await request(app)
       .post('/topics')
       .type('form')
-      .set('Cookie', signInCookie)
+      .set('Cookie', authCookie)
       .send({ title: 'post title', body: 'post body' });
     const url = res1.headers.location.match(/\/topics\/\d+/)[0];
     const res2 = await request(app)
       .patch(url)
-      .set('Cookie', signInCookie);
+      .set('Cookie', authCookie);
     expect(res2).toHaveHTTPStatus(422);
   });
 
   it('DELETE topics/:id', async () => {
     const app = getApp();
+    const authCookie = await signIn(app, { email: 'admin@admin', password: 'qwerty' });
     const res1 = await request(app)
       .post('/topics')
       .type('form')
-      .set('Cookie', signInCookie)
+      .set('Cookie', authCookie)
       .send({ title: 'post title', body: 'post body' });
     const url = res1.headers.location.match(/\/topics\/\d+/)[0];
     const res2 = await request(app)
       .delete(url)
-      .set('Cookie', signInCookie);
+      .set('Cookie', authCookie);
     expect(res2).toHaveHTTPStatus(302);
   });
 
   it('DELETE topics/:id (unauthorized)', async () => {
     const app = getApp();
+    const authCookie = await signIn(app, { email: 'admin@admin', password: 'qwerty' });
     const res1 = await request(app)
       .post('/topics')
       .type('form')
-      .set('Cookie', signInCookie)
+      .set('Cookie', authCookie)
       .send({ title: 'post title', body: 'post body' });
     const url = res1.headers.location.match(/\/topics\/\d+/)[0];
     const res2 = await request(app)
