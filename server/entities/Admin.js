ab18024159bb4e6b80a3384be3c9c0fb47b9dbb2
Aleksandr Saitgalin <cred1tor@yandex.ru>
2021-11-30 18:58:04 +0600

    fix id working for both types, add class properties

--- server/entities/Admin.js
+++ server/entities/Admin.js
@@ -1,8 +1,5 @@
 import User from './User.js';
 
 export default class Admin extends User {
-  constructor(email, name, password) {
-    super(email, name, password);
-    this.admin = true;
-  }
+  admin = true;
 }
--- server/entities/User.js
+++ server/entities/User.js
@@ -1,15 +1,19 @@
 import encrypt from '../utils/encrypt.js';
 
-class User {
+let id = 1;
+
+export default class User {
+  guest = false;
+
+  admin = false;
+
   constructor(email, name, password) {
-    this.id = this.constructor.id;
-    this.constructor.id += 1;
+    this.id = id;
+    id += 1;
     this.email = email;
     this.name = name;
     this.passwordDigest = encrypt(password);
     this.registrationDate = Date.now();
-    this.guest = false;
-    this.admin = false;
   }
 
   isGuest() {
@@ -24,6 +28,3 @@ class User {
     return encrypt(password) === this.passwordDigest;
   }
 }
-
-User.id = 1;
-export default User;
