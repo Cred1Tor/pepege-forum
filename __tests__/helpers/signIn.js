e61ba9d1e9893c7c8c0f0f1d9baa96a525161d32
Aleksandr Saitgalin <cred1tor@yandex.ru>
2021-11-30 15:02:08 +0600

    fix route in signIn() function

--- __tests__/helpers/signIn.js
+++ __tests__/helpers/signIn.js
@@ -2,7 +2,7 @@ import request from 'supertest';
 
 export default async (app, userData) => {
   const response = await request(app)
-    .post('/session/new')
+    .post('/session')
     .type('form')
     .send(userData);
   return response.headers['set-cookie'];
