ed6bdbc817be7b8e82004fa526ea74f1b6538cb9
Aleksandr Saitgalin <cred1tor@yandex.ru>
2021-11-30 18:05:25 +0600

    fix wrong currentUser address

--- server/routes/topics.js
+++ server/routes/topics.js
@@ -14,8 +14,7 @@ export default (app) => {
   const authorizeForEdition = (req, res, next) => {
     const topic = app.models.topics.find((t) => t.id.toString() === req.params.id);
 
-    if (topic.creator.id === res.currentUser.id || res.currentUser.isAdmin()) {
-      next();
+    if (topic.creator.id === res.locals.currentUser.id || res.locals.currentUser.isAdmin()) {
     }
 
     next(new app.httpError.Forbidden('You are not authorized to edit this topic'));
