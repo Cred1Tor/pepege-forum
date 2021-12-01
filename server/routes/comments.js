01fbafa5bf420ece0fbc3357b2922a8532b4feea
Aleksandr Saitgalin <cred1tor@yandex.ru>
2021-12-01 20:17:52 +0600

    add comments route module

---
+++ server/routes/comments.js
@@ -0,0 +1,56 @@
+import {
+  requiredAuth,
+  verifyTopicId,
+  verifyCommentId,
+  authorizeForCommentEdition,
+} from '../utils/middlewares.js';
+
+export default (app) => {
+  app.post('/topics/:topicId/comments', requiredAuth, verifyTopicId, (req, res) => {
+    const topic = app.models.topics.find((t) => t.id.toString() === req.params.topicId);
+    const { body } = req.body;
+    const errors = {};
+
+    if (!body) {
+      errors.body = "Comment can't be blank";
+    }
+
+    if (Object.keys(errors).length === 0) {
+      const comment = topic.addComment(body, res.locals.currentUser);
+      res.set('Comment-Id', comment.id);
+      res.status(201).render(`/topics/${topic.id}`, { topic, commentForm: {}, errors: {} });
+      return;
+    }
+
+    res.status(422);
+    res.render('topics/show', { topic, commentForm: req.body, errors });
+  });
+
+  app.patch('/topics/:topicId/comments/:commentId', verifyCommentId, authorizeForCommentEdition, (req, res) => {
+    const topic = app.models.topics.find((t) => t.id.toString() === req.params.topicId);
+    const { body } = req.body;
+    const errors = {};
+
+    if (!body) {
+      errors.body = "Comment can't be blank";
+    }
+
+    if (Object.keys(errors).length === 0) {
+      const comment = topic.findComment(Number(req.params.commentId));
+      comment.edit(body, res.locals.currentUser);
+      res.render('/topics/show', { topic, commentForm: {}, errors: {} });
+      return;
+    }
+
+    res.status(422);
+    res.render(`/topics/${topic.id}`, { topic, commentForm: req.body, errors });
+  });
+
+  app.delete('/topics/:topicId/comments/:commentId', verifyCommentId, authorizeForCommentEdition, (req, res) => {
+    const topic = app.models.topics.find((t) => t.id.toString() === req.params.topicId);
+    topic.deleteComment(Number(req.params.commentId));
+    res.status.render('topics/show', { topic, commentForm: {}, errors: {} });
+  });
+
+  return app;
+};
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,12 +2,14 @@ import users from './users.js';
 import session from './session.js';
 import welcome from './welcome.js';
 import topics from './topics.js';
+import comments from './comments.js';
 
 const controllers = [
   welcome,
   users,
   session,
   topics,
+  comments,
 ];
 
 export default (app) => controllers.forEach((f) => f(app));
